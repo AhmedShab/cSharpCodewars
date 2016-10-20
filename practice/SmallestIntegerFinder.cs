58937efacbbc2473b1788da608f5ff8b11e733f1
AhmedShab <ahmed.vuw@gmail.com>
2016-10-20 16:31:27 +1300

    addded my first practice

---
+++ practice/SmallestIntegerFinder.cs
@@ -0,0 +1,8 @@
+public class SmallestIntegerFinder
+{
+    // Given [34, 15, 88, 2] your solution will return 2
+    public static int FindSmallestInt(int[] args)
+    {
+        int small = args.
+    }
+}
